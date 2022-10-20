d4a9e3d7febe2b8c5d0661ecf0a25a205025c86d
Margaret <y.margaret.lu+github@gmail.com>
2022-10-20 11:40:15 -0700

    finished route setup and return list of all planets

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+planet_bp = Blueprint("planet_bp",__name__,url_prefix="/planets")
+
+class Planet():
+    def __init__(self, id, name, description, population):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.population = population
+
+planets = [Planet(1,"Mercury","It's solid",0),
+        Planet(2,"Mars","It's red",1000000),
+        Planet(3,"Earth","The blue marble",8000000000)]
+
+@planet_bp.route('', methods=['GET'])
+def get_all_planets():
+    planet_list=[]
+    for planet in planets:
+        planet_list.append({
+            "id":planet.id,
+            "name":planet.name,
+            "description":planet.description,
+            "population":planet.population
+        })
+    return jsonify(planet_list)
